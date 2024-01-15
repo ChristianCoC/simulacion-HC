6349e1e9fe416fce2a9deac054ea55ccfb77412d
Christian CoC <christianelmillo81@gmail.com>
2024-01-15 08:51:50 -0300

    Simulacion Henry Challenge

---
+++ 03.js
@@ -0,0 +1,22 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumentos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la función 
+*/
+function buscarAmigo(amigos, nombre) {
+  // La función llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
+  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
+  // recibe un string llamado 'nombre'.
+  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
+  // Ej:
+  //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
+  //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
+
+  // Tu código aca:
+  return amigos.find((element) => element.nombre === nombre);
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = buscarAmigo
---
+++ 09.js
@@ -0,0 +1,40 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumentos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la función 
+*/
+function filtrar(funcion) {
+  // Escribí una función filtrar en el prototipo de Arrays,
+  // que recibe una función (callback) que devuelve true o false.
+  // filtrar los elementos de ese arreglo en base al resultado de esa función
+  // comparadora, devolver un nuevo arreglo con los elementos filtrados.
+  // NO USAR LA FUNCIÓN FILTER DE LOS ARREGLOS.
+  // ej:
+  // var productos = [{
+  //   price: 100,
+  //   name: 'tv'
+  // }, {
+  //   price: 50,
+  //   name: 'phone'
+  // }, {
+  //   price: 30,
+  //   name: 'lamp'
+  // }]
+  // productos.filtrar(function(p) {
+  //   return p.price >= 50;
+  // }) => [{price: 100, name:'tv'}]
+  Array.prototype.filtrar = function(callback) {
+    const array = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i])) {
+        array.push(this[i]);
+      }
+    }
+    return array;
+  } 
+};
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = filtrar
